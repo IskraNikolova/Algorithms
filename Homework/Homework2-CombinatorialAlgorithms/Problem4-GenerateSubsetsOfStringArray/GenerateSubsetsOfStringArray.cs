5f2ce4b9ecb1e702437a4243ef2fe4013eeeb5b8
IskraNikolova <air_iskra@yahoo.com>
2015-10-04 16:04:51 +0300

    Add a project

---
+++ Homework/Homework2-CombinatorialAlgorithms/Problem4-GenerateSubsetsOfStringArray/GenerateSubsetsOfStringArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class GenerateSubsetsOfStringArray
+{
+    public static void Main()
+    {
+    
+        string[] input = new[] {"test", "rock", "fun"};
+
+        int k = 2;
+        string[] combination = new string[k];
+
+
+        GeneratingCombinations(combination, input, 0, 0);
+    }
+
+    private static void GeneratingCombinations(string[] combination, string[] input, int index, int startIndex)
+    {
+        if (index == combination.Length)
+        {
+            Console.WriteLine("(" + string.Join(" ", combination) + ")");
+        }
+        else
+        {
+            for (int i = startIndex; i < input.Length; i++)
+            {
+                combination[index] = input[i];
+                GeneratingCombinations(combination, input, index + 1, i + 1);
+            }
+        }
+    }
+}
+
